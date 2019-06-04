cf75dd0eabff21c986b2bc76f33aaf53ff94c773
Joberth Rogers <joberth.rogers18@gmail.com>
2019-06-04 17:58:29 -0400

    Added slider to screen  and generate ponint underneath the borders

--- src/sketch.js
+++ src/sketch.js
@@ -3,24 +3,39 @@ var height = window.innerHeight - 110;
 
 console.log(width, height)
 
-let input, button, gretting;
+let input, button, labelInput;
 let x_axis = [];
 let y_axis = [];
 
 function setup() {
-    var canvas = createCanvas(window.innerWidth - 100, window.innerHeight - 100);
+    var canvas = createCanvas(window.innerWidth - 20, window.innerHeight - 100);
     canvas.parent("sketch");
     noStroke();
 
-    gretting = createElement('h5', 'Digite a quantidade de pontos: ');
-    gretting.position(50, 7);
+    //Input number of points
+    labelInput = createElement('h5', 'Digite a quantidade de pontos: ');
+    labelInput.position(50, 7);
 
     input = createInput();
-    input.position(gretting.x + input.width + 20, 25);
+    input.position(labelInput.x + input.width + 20, 25);
 
     button = createButton('submit');
     button.position(input.x + input.width + 10, 25);
     button.mousePressed(listenner);
+
+    //Slide for adjust the velocity
+
+    VelocitySlider = createSlider(10, 100, 10);
+    VelocitySlider.position( 900, input.y);
+
+    labelVelocity = createElement('h5', 'Velocidade: ');
+    labelVelocity.position(750 , 7);
+
+    labelVelocity = createElement('h5', '10 ms');
+    labelVelocity.position(VelocitySlider.x - 45 , 7);
+
+    labelVelocity = createElement('h5', '100 ms');
+    labelVelocity.position(VelocitySlider.x + VelocitySlider.width + 10 , 7);
 
     textAlign(CENTER);
     textSize(50);
@@ -34,10 +49,10 @@ function listenner() {
     input.value("");
 
     while (x_axis.length < number_points) {
-        let current_x = Math.floor(Math.random() * width - 10) + 10;
+        let current_x = Math.floor(Math.random() * (width - 15)) + 10;
         if (x_axis.indexOf(current_x) === -1) {
             x_axis.push(current_x);
-            y_axis.push(Math.floor(Math.random() * height - 15) + 15);
+            y_axis.push(Math.floor(Math.random() * (height - 15)) + 10);
         }
     }
 
